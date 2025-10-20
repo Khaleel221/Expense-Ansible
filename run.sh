a1dcdce85c27c1ebbe373a346d6a7eb8d74106dd
Khaleel <khaleelshaik711@gmail.com>
2025-10-20 16:42:11 +0530

    pipelines for frontend and backend and mysql

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 if [ "$pipeline" == "true" ]; then
   exit
 fi
-
+##
 ansible-playbook -i ${component}-${env}.khaleel221.shop, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
