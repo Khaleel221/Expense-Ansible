9cb50fa31667207ede7ed1371afcb73068e32a04
Khaleel <khaleelshaik711@gmail.com>
2025-10-20 15:59:36 +0530

    pipelines for frontend and backend and mysql

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-#ansible-playbook -i ${component}-${env}.khaleel221.shop, -e ansible_user=ec2-user -e ansible_pasword=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
-ansible --version
+ansible-playbook -i frontend-dev.khaleel221.shop, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
